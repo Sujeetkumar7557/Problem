515ca227c70e0d5cf8c64de97a6ce47c42f07740
Sujeetkumar7557 <sy7449850@gmail.com>
2026-09-25 22:16:51 +0530

    Time: 0 ms (100%), Space: 13.8 MB (23.86%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void getPerms(vector<int>& nums, int idx, vector<vector<int>>& ans) {
+        if (idx == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+
+        set<int> used;
+
+        for (int i = idx; i < nums.size(); i++) {
+            if (used.count(nums[i])) continue;
+
+            used.insert(nums[i]);
+
+            swap(nums[idx], nums[i]);
+            getPerms(nums, idx + 1, ans);
+            swap(nums[idx], nums[i]);
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        getPerms(nums, 0, ans);
+        return ans;
+    }
+};
